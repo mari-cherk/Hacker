7682746870e9d06dc7047d0394593ba085f69a27
Marina Cherkasova <1mari.cherkasova@gmail.com>
2020-08-24 21:32:32 +0200

    Factory Pattern

--- src/main/java/Reflection/CanYouAccess.java
+++ src/main/java/Reflection/CanYouAccess.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.security.Permission;
 
+
+
 public class CanYouAccess {
 
     public static void main(String[] args) throws Exception {
